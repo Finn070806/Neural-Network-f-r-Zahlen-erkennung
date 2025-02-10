8a174f18eb8ec509de8145a1b73914b11ab2fd2c
Finn.laptop <Finnroock6@gmail.com>
2025-02-10 15:06:35 +0100

    1

--- test.py
+++ test.py
@@ -9,9 +9,10 @@ def sigmoid_derivative(x):
 training_input = np.array([ [0,0,1],
                             [1,1,1],
                             [1,0,1],
-                            [0,1,1]])
+                            [0,1,1],
+                            [1,0,0]])
 
-training_outputs = np.array([[0,1,1,0]]).T
+training_outputs = np.array([[0,1,1,0,1]]).T
 
 np.random.seed(1)
 
@@ -20,7 +21,7 @@ synaptic_weights = 2 * np.random.random((3,1)) - 1
 print("Random starting synaptic weights: = ")
 print(synaptic_weights)
 
-for iteration in range(10000):
+for iteration in range(100000):
 
     input_layer = training_input
 
